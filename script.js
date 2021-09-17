459c5302fabef6dddcb017968d369b47d81ceecb
angelop47 <pescettoangelo@gmail.com>
2021-09-16 23:29:15 -0300

    .

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["programmer analyst student", "front end developer"],
+        strings: ["Programmer Analyst Student", "Front-End Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["programmer analyst student", "front end developer"],
+        strings: ["Programmer Analyst Student", "Front-End Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
