c41f3810b53220ea2190efd725cb5e48f15cfbdd
angelop47 <69943997+angelop47@users.noreply.github.com>
2022-08-01 20:46:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer Analyst Student", "Front-End Developer", "Autodidact"],
+        strings: ["Front-end developer", "Back-end developer", "Mobile Developer", "Autodidact"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Programmer Analyst Student", "Front-End Developer", "Autodidact"],
+        strings: ["Front-end developer", "Back-end developer", "Mobile Developer", "Autodidact"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
